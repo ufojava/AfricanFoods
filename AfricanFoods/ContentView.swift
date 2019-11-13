85a40af6c0351fb1728fc42886fa287a3208fa1b
Ufuoma Okoro <ufookoro@gmail.com>
2019-11-13 23:24:12 +0000

    (1) AfricanFood Menu Begin

--- AfricanFoods/ContentView.swift
+++ AfricanFoods/ContentView.swift
@@ -10,7 +10,61 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        
+        NavigationView {
+            
+            Section {
+            VStack(alignment: .leading) {
+                
+                        
+                Text("Meal Description").foregroundColor(.green)
+                Spacer()
+                           
+                        NavigationLink(destination: PoundedYam()) {
+                            Text("Pounded Yam")
+                }
+                
+                        NavigationLink(destination: YamPepSoup()) {
+                            Text("Yam and Pepper Soup")
+                        }
+                
+                
+                    NavigationLink(destination: JollofRice()) {
+                            Text("Jollof Rice")
+                    }
+                        
+                Spacer()
+                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
+                
+                
+                
+                
+                
+                
+                }.padding()
+                
+            }
+             
+            
+            Section {
+                
+                Image("africanfoods")
+                    .resizable()
+                    .frame(width: 275.0, height: 250.0)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.black, lineWidth: 5))
+                    .scaledToFit()
+                    
+                    
+                
+                
+                
+                
+            }
+          
+            .navigationBarTitle(Text("Menu"))
+        }
+        
     }
 }
 
@@ -19,3 +73,33 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+struct PoundedYam: View {
+    
+    var body: some View {
+        
+        Text("Page 1")
+    }
+    
+}
+    
+struct YamPepSoup: View {
+        
+    var body: some View {
+            
+        Text("Page 2").fontWeight(.heavy)
+        }
+        
+    }
+
+struct JollofRice: View {
+    
+var body: some View {
+        
+    Text("Page 2").fontWeight(.heavy)
+    }
+    
+}
+    
+
