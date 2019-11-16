2f48db7d3a8bbdafb4bee389a0e307058e4524f1
Ufuoma Okoro <ufookoro@gmail.com>
2019-11-16 16:33:13 +0000

    Completed menu UI and description

--- AfricanFoods/ContentView.swift
+++ AfricanFoods/ContentView.swift
@@ -121,36 +121,102 @@ struct MenuOption: View {
     
         VStack(alignment: .leading) {
             
-            Section(header: Text("Select Meal")) {
+            Section(header: Text("Select Meal").padding(.horizontal, 25)) {
                 
                 Picker("Select Item", selection: $menuItem) {
                              ForEach(0 ..< menuItems.count) {
                                  Text(self.menuItems[$0])
-                                 //Spacer()
+        
+                                
                                  
                              }
                          }.pickerStyle(SegmentedPickerStyle())
+                          .padding(.horizontal, 25)
+                    
                          
                          
+               
                          if menuItems[menuItem] == "Rice Meals" {
-                             Text("Selected Rice Meals")
+                             Text("Rice Meals:")
+                                .foregroundColor(.black)
+                                .padding(.horizontal, 25)
+                            
+                            Spacer().frame(height:20)
+                                Text("Rice meals consist of Jollof Rice, Rice and Stew, Fried Rice. You can option in meat, fish and or plantain with any of the meals")
                                 .foregroundColor(.blue)
+                                .lineLimit(nil)
+                                .padding(.horizontal, 25)
+                                
+                                Spacer()
+                            
+                                //Insert image - Jollof Rice
+                                Image("jollofRice")
+                                    .resizable()
+                                    .frame(width:250, height: 250)
+                                    .scaledToFit()
+                                    .border(Color.black, width: 4)
+                                    .padding(.horizontal, 100)
+                            
+                                
+                                
+                
                              
                          } else if menuItems[menuItem] == "Swallow Meals" {
-                             Text("Selected Swallow")
+                             Text("Swallow")
                                 .foregroundColor(.green)
+                                .padding(.horizontal, 25)
+                            
+                            Spacer().frame(height:20)
+                                Text("Swallow is the colloqual word for Pounded Yam, Eba, Fufu or Amala. This is complemented with soups, such as Egusi, Okra etc. You could have this meal with either meat or fish")
+                                
+                                    .foregroundColor(.blue)
+                                    .lineLimit(nil)
+                                    .padding(.horizontal, 25)
+                                    
+                                Spacer()
+                            
+                                //Inset Image
+                                Image("poundedYamEgusi")
+                                    .resizable()
+                                    .frame(width:250, height:250)
+                                    .scaledToFit()
+                                    .border(Color.black, width: 4)
+                                    .padding(.horizontal, 100)
+                            
                              
                              
                          } else if menuItems[menuItem] == "Pepper Soups" {
-                             Text("Pepper Soups Selected")
+                             Text("Pepper Soup")
                                 .foregroundColor(.red)
+                                .padding(.horizontal, 25)
+                            
+                            Spacer().frame(height:20)
+                                Text("Pepper soup is normally taken after one has had either Pounded Yam or rice meals. Depending on the audeience, the soup can be lightly spicy to very hot. Pepper soup can be had with either fish or meat")
+                            
+                                    .foregroundColor(.blue)
+                            .lineLimit(nil)
+                                    .padding(.horizontal, 25)
+                            
+                            Spacer()
+                            
+                            //Insert Image
+                            Image("pepperSoupYam")
+                                .resizable()
+                                .frame(width:250, height:250)
+                                .scaledToFit()
+                                .border(Color.black, width: 4)
+                                .padding(.horizontal, 100)
+                            
                          }
-                         
+           
+                     
                          Spacer()
                 
                 
             }.font(.system(size: 14))
-             .foregroundColor(.green)
+             .foregroundColor(.purple)
+    
+    
             
             
          
